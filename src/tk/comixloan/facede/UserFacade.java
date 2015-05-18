16a9b3c4ed87cc60df8b2a8d82a2c17148c90645
Alessandro Oddi <Alessandro@MacBook-Pro-di-Alessandro.local>
2015-05-18 10:02:23 +0200

    aggiunta facade di user operazioni creud parziali senza commenti

--- src/tk/comixloan/facede/UserFacade.java
+++ src/tk/comixloan/facede/UserFacade.java
@@ -1,41 +1,18 @@
 package tk.comixloan.facede;
 
 import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.persistence.Column;
 import javax.persistence.EntityManager;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.PersistenceContext;
-
 import tk.comixloan.model.Community;
 import tk.comixloan.model.HistoryLoan;
 import tk.comixloan.model.Loan;
 import tk.comixloan.model.User;
 import tk.comixloan.model.Volume;
 
-
-//@Column(nullable=false)
-//private String email;
-//
-//@Column(nullable=false)
-//private String password;
-//
-//@Column(nullable=false)
-//private String userName;
-//
-//@OneToMany
-//private List<Volume> volumes;
-//
-//@OneToMany
-//private List<HistoryLoan> historiesLoan;
-//
-//@OneToMany
-//private List<Loan> loans;
-//
-//@ManyToMany
-//private List<Community> communities;
 
 @Stateless
 public class UserFacade {
