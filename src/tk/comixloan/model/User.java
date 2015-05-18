649a5f417a4787e48c7a9fa427f7a3f2b76a38a4
Alessandro Oddi <Alessandro@MacBook-Pro-di-Alessandro.local>
2015-05-18 09:14:31 +0200

    creazione utente model

---
+++ src/tk/comixloan/model/User.java
@@ -0,0 +1,106 @@
+package tk.comixloan.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Id id;
+
+	@Column(nullable=false)
+	private String name;
+	
+	@Column(nullable=false)
+	private String surName;
+	
+	@Column(nullable=false)
+	private String email;
+	
+	@Column(nullable=false)
+	private String password;
+	
+	@Column(nullable=false)
+	private String userName;
+	
+	@OneToMany
+	private List<Volume> volumes;
+	
+	@OneToMany
+	private List<HistoryLoan> historiesLoan;
+	
+	@OneToMany
+	private List<Loan> loans;
+
+	
+	
+	public User(String name, String surName, String email, String password,
+			String userName) {
+		super();
+		this.name = name;
+		this.surName = surName;
+		this.email = email;
+		this.password = password;
+		this.userName = userName;
+	}
+	
+	public Id getId() {
+		return id;
+	}
+
+	public void setId(Id id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+	
+}
