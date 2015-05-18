c1da9098f7589806ad766e1b9ea4e20ea36fb8f5
Alessandro Oddi <Alessandro@MacBook-Pro-di-Alessandro.local>
2015-05-18 09:02:32 +0200

    creazione prestito

--- src/tk/comixloan/model/Loan.java
+++ src/tk/comixloan/model/Loan.java
@@ -1,5 +1,22 @@
 package tk.comixloan.model;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
 public class Loan {
+	private Date startDate;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Id id;
+	@OneToMany
+	private List<Volume> volumes;
+	
+	
+	public Loan(Date date){
+		this.startDate= date;
+	}
 
 }
