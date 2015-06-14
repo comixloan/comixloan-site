72c720d994d6e160b785df7bf6c4fa34748528db
Matteo Filippi <info@mfilippi.guru>
2015-06-14 19:16:11 +0200

    Correzione errore nomi

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -1,8 +1,8 @@
 $(function() {
 	var $window = $(window);
-	var setMaxHeight = function() {
+	var setHeight = function() {
 		$('#autoscroll').css('height', $window.height() - 162);
 	}
-	$window.on('resize', setMaxHeight);
-	setMaxHeight();
+	$window.on('resize', setHeight);
+	setHeight();
 });
