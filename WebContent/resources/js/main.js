e32f0b88aa15e55647e80bc21ac279a73912561d
Matteo Filippi <info@mfilippi.guru>
2015-06-15 12:50:14 +0200

    Cose fighe

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -5,4 +5,18 @@ $(function() {
 	}
 	$window.on('resize', setHeight);
 	setHeight();
+	
+	var logoChange = function (){
+		var logoImage = Math.random() * 3;
+		logoImage = parseInt(logoImage, 10);
+		
+		$("#imagelogo").removeClass();
+		$("#imagelogo").addClass("logos-"+ logoImage);
+	}
+	
+	$("#imagelogo").click(function (){
+		logoChange();
+	});
+	
+	logoChange();
 });
